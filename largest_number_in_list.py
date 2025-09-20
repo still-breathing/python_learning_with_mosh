03fa54184cf0c7e9f9a7a01dba9d58ccfa790de9
raiyeemfarhan <raiyeem@cefalo.com>
2025-09-20 16:57:02 +0600

    clean up

--- largest_number_in_list.py
+++
@@ -1,9 +0,0 @@
-print("Largest number in a list")
-
-numbers = [3, 6, 2, 8, 4, 10, 12, 7]
-largest = numbers[0]  # assume the first number is the largest initially
-for number in numbers:
-    if number > largest:
-        largest = number
-print(f"The largest number in the list {numbers} is: \n{largest}")
-print("\nEND\n")
