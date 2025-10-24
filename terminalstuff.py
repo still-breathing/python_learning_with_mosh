936d76eaac0141b3e58373c1af758db9b6b201a6
raiyeemfarhan <raiyeem@cefalo.com>
2025-10-24 16:16:57 +0600

    added vscode launch & settings json

---
+++ terminalstuff.py
@@ -0,0 +1,7 @@
+from rich.console import Console
+from rich.markdown import Markdown
+from rich import box
+
+data = [2,3,2,5,6,11,2,7,9,3,1,0,3]
+console = Console()
+console.print("Kisu ekta", style="bold red")
